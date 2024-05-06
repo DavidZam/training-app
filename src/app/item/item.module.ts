27094dc1ae10262bacd8f7ecf95189c8870346f9
David Zamora <davidzamorarey@gmail.com>
2024-05-06 14:41:52 +0200

    feat: :iphone: angular flexLayout added to apply responsiveness to the item-list page

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
 import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [ItemListComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, FlexModule],
 })
 export class ItemModule {}
