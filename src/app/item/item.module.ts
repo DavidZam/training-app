6ef19d05ddf3d21f277137e7b2185f7dd589bfab
David Zamora <davidzamorarey@gmail.com>
2024-05-06 14:37:13 +0200

    feat: :bento: item assets added and item-list template updated

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -1,15 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { ItemListComponent } from './item-list/item-list.component';
 
-
-
 @NgModule({
-  declarations: [
-    ItemListComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ItemListComponent],
+  imports: [CommonModule, MatCardModule],
 })
-export class ItemModule { }
+export class ItemModule {}
